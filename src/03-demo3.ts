fabfdafbd732e954022ad05e5c3a5b248695cd92
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-02-28 15:54:05 -0600

    alcaldia 28/2/23

--- src/03-demo3.ts
+++ src/03-demo3.ts
@@ -1,8 +1,9 @@
-let  myProductName = 'product 1';
+(()=>{let  myProductName = 'product 1';
 let myProductPrice = 5;
 myProductName = 'producto 2';
 myProductName.toUpperCase();
 myProductPrice.toFixed();
 
 const myProductStock = 1000;
-const myProductName2 = 'Producto 1';
+const myProductName2 = 'Producto 1';})();
+
