7ce882fe9adcb50be884726aab61de667fba0b83
iDannyGR <86504340+iDannyGR@users.noreply.github.com>
2023-03-07 15:57:22 -0600

    alcaldia 7/3/23

--- advance/src/01-class.ts
+++ advance/src/01-class.ts
@@ -5,3 +5,8 @@ date.getFullYear();
 const date2 = new Date(1993, 1 , 12) // 0 es enero
 date2.getHours()
 console.log(date2)
+
+
+
+const myDate = new MyDate(2021,3,12);
+console.log(myDate)
---
+++ advance/src/02-method.ts
@@ -0,0 +1,17 @@
+class MyDate {
+  year:number;
+  month: number;
+  day:number;
+
+  constructor(year:number, month:number, day:number){
+    this.year= year,
+    this.month = month,
+    this.day = day
+  }
+  printFormat():string{
+    
+
+    return'';
+  }
+
+}
